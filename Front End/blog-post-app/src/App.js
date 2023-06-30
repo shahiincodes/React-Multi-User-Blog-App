39d6cf68d11d94692240bbc932339f29eba578e3
Habib <shahin.amla@gmail.com>
2023-06-30 19:42:48 +0530

    Top component added

--- Front End/blog-post-app/src/App.js
+++ Front End/blog-post-app/src/App.js
@@ -1,9 +1,11 @@
+import Header from "./Header/Header";
 import Top from "./Top/Top";
 
 function App() {
   return (
     <div className="App">
       <Top />
+      <Header />
     </div>
   );
 }
--- Front End/blog-post-app/src/Top/Top.js
+++ Front End/blog-post-app/src/Top/Top.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import './Top.css'
+import img from "./16824922819521671634744497WIN_20220817_17_10_30_Pro.jpg"
 
 function Top() {
   return (
     <div className='top'>
-        <div className="item1">1</div>
-        <div className="item2">2</div>
-        <div className="item3">3</div>
+        <div className="item1">
+        <i class="socialIcons fa-brands fa-instagram"></i>
+        <i class="socialIcons fa-brands fa-facebook"></i>
+        <i class="socialIcons fa-brands fa-linkedin"></i>
+        <i class="socialIcons fa-brands fa-twitter"></i>
+        <i class="socialIcons fa-brands fa-github"></i>
+        </div>
+        <div className="item2">
+            <ul>
+                <li className="topItems">Home</li>
+                <li className="topItems">About</li>
+                <li className="topItems">Blogs</li>
+                <li className="topItems">Contact</li>
+            </ul>
+        </div>
+        <div className="item3">
+            <img src={img} alt="" />
+            <input type="text" />
+            <i class="searchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
     </div>
   )
 }
