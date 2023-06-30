f0d909aa613bd9446db4788a6a2623657b214321
Habib <shahin.amla@gmail.com>
2023-06-30 18:09:02 +0530

    initial commit

---
+++ Front End/blog-post-app/src/App.js
@@ -0,0 +1,11 @@
+import Top from "./Top/Top";
+
+function App() {
+  return (
+    <div className="App">
+      <Top />
+    </div>
+  );
+}
+
+export default App;
---
+++ Front End/blog-post-app/src/Top/Top.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Top.css'
+
+function Top() {
+  return (
+    <div className='top'>
+        <div className="item1">1</div>
+        <div className="item2">2</div>
+        <div className="item3">3</div>
+    </div>
+  )
+}
+
+export default Top
